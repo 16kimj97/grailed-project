5f16ef857d96b7341bfce2c9ecf1b70f448e4352
Justin <16kimj97@gmail.com>
2024-04-25 21:01:25 -0700

    added updateClothing thunk

--- project/react-vite/src/redux/clothing.js
+++ project/react-vite/src/redux/clothing.js
@@ -1,5 +1,6 @@
 const FETCH_CLOTHING = 'FETCH_CLOTHING'
 const FETCH_CLOTHING_BY_ID = 'FETCH_CLOTHING_BY_ID'
+const UPDATE_CLOTHING = 'UPDATE_CLOTHING'
 
 export const fetchClothing = clothing => ({
     type: FETCH_CLOTHING,
@@ -8,6 +9,11 @@ export const fetchClothing = clothing => ({
 
 export const fetchClothingById = clothing => ({
     type: FETCH_CLOTHING_BY_ID,
+    payload: clothing
+})
+
+export const updateClothing = clothing => ({
+    type: UPDATE_CLOTHING,
     payload: clothing
 })
 
@@ -30,6 +36,21 @@ export const thunkFetchClothing = () => async dispatch => {
     }
 }
 
+export const thunkUpdateClothing = (clothing, id) =>  async dispatch => {
+    const res = await fetch(`/api/clothings/${id}`,{
+        method: 'PUT',
+        body: song
+    })
+
+    if (!Response.ok) {
+        throw new Error('Failed to update the item')
+    }
+
+    const updatedClothing = await response.json();
+    dispatch(updateClothing(updatedClothing))
+    return updatedClothing
+}
+
 const clothingReducer = (state = {}, action) => {
     switch (action.type) {
         case FETCH_CLOTHING: {
@@ -44,6 +65,11 @@ const clothingReducer = (state = {}, action) => {
                 ...state,
                 [action.payload.id]: action.payload
             }
+        case UPDATE_CLOTHING: {
+            const editClothingState = {...state}
+            editClothingState[action.payload.id] = action.payload
+            return editClothingState
+        }
         default:
             return state;
     }
