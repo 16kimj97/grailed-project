0874aab3ecbabf49b9a9879e671a4b5669d9faf5
Justin <16kimj97@gmail.com>
2024-07-05 11:05:19 -0700

    wishlist routes

--- app/api/routes/wishlist_routes.py
+++ app/api/routes/wishlist_routes.py
@@ -2,3 +2,5 @@ from flask import Blueprint, jsonify, request
 from app.models import WishList, db
 from flask_login import current_user, login_required
 from datetime import datetime
+
+wishlist_routes = Blueprint('wishlist', __name__)
