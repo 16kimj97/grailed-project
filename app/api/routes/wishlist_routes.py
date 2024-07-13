866e1b800f7af1a925a2f5c83fea660459b0d497
Justin <16kimj97@gmail.com>
2024-07-12 20:21:55 -0700

    get all wishlists

--- app/api/routes/wishlist_routes.py
+++ app/api/routes/wishlist_routes.py
@@ -4,3 +4,9 @@ from flask_login import current_user, login_required
 from datetime import datetime
 
 wishlist_routes = Blueprint('wishlist', __name__)
+
+@wishlist_routes.routes('/', methods=['GET'])
+@login_required
+def get_all_wishlists():
+    wishlists = WishList.query.all()
+    return jsonify([wishlist.to_dict() for wishlist in wishlists])
