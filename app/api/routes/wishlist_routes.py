896dbf9d8494cedaac08e2cd3a3e457eb8633346
Justin <16kimj97@gmail.com>
2024-07-17 14:06:03 -0700

    close out

--- app/api/routes/wishlist_routes.py
+++ app/api/routes/wishlist_routes.py
@@ -1,18 +1,18 @@
-from flask import Blueprint, jsonify, request
-from app.models import WishList, db
-from flask_login import current_user, login_required
-from datetime import datetime
+# from flask import Blueprint, jsonify, request
+# from app.models import WishList, db
+# from flask_login import current_user, login_required
+# from datetime import datetime
 
-wishlist_routes = Blueprint('wishlist', __name__)
+# wishlist_routes = Blueprint('wishlist', __name__)
 
 
-@wishlist_routes.route('/', methods=['GET'])
-@login_required
-def get_wishlists_by_user():
-    user_id = current_user.id
-    wishlists = WishList.query.filter_by(user_id=user_id).all()
-    return jsonify([wishlist.to_dict() for wishlist in wishlists])
+# @wishlist_routes.route('/', methods=['GET'])
+# @login_required
+# def get_wishlists_by_user():
+#     user_id = current_user.id
+#     wishlists = WishList.query.filter_by(user_id=user_id).all()
+#     return jsonify([wishlist.to_dict() for wishlist in wishlists])
 
-@wishlist_routes.route('/delete')
-@login_required
-def delete_wishlists():
+# @wishlist_routes.route('/delete')
+# @login_required
+# def delete_wishlists():
